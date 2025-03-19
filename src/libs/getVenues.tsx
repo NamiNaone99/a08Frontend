b759f3e5732531132a588c495947a416ecc73b35
NamiNaone99 <nongft09090@gmail.com>
2025-03-19 22:34:36 +0700

    change url

--- src/libs/getVenues.tsx
+++ src/libs/getVenues.tsx
@@ -1,7 +1,9 @@
 export default async function getVenues(): Promise<VenueJson> {
   return new Promise((resolve, reject) => {
     setTimeout(async () => {
-      const response = await fetch("hs");
+      const response = await fetch(
+        "https://a08-frontend-3zfm.vercel.app/api/v1/venues"
+      );
       if (!response.ok) {
         reject(new Error("Failed to fetch venues"));
       }
